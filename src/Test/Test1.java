943a7556853c0c3bf738daaa30d51d2791cd56e2
dongguangwen <passion3664@163.com>
2019-12-05 17:40:59 +0800

    第四次提交测试

--- src/Test/Test1.java
+++ src/Test/Test1.java
@@ -5,11 +5,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("11111111111111111111");
-		System.out.println("11111111111111111111");
-		System.out.println("11111111111111111111");
-		System.out.println("11111111111111111111");
-		System.out.println("11111111111111111111");
-		System.out.println("11111111111111111111");
 	}
 
 }
